0e191acb1936af3a27fd44ed3c976199f0901989
Veronica Zheng <veronica.zheng@coinbase.com>
2019-08-25 15:17:34 -0700

    configure drizzle

--- src/main.js
+++ src/main.js
@@ -7,7 +7,11 @@ import BootstrapVue from "bootstrap-vue";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import drizzleVuePlugin from '@drizzle/vue-plugin'
+import drizzleOptions from './drizzleOptions'
+
 Vue.use(BootstrapVue);
+Vue.use(drizzleVuePlugin, { store, drizzleOptions })
 Vue.config.productionTip = false;
 
 new Vue({
