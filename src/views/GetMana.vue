fe5f4bfabd1a26ac1a0a62ad2a9edc0128593a33
Veronica Zheng <veronica.zheng@coinbase.com>
2019-08-31 14:59:02 -0700

    add getWizards

--- src/views/GetMana.vue
+++ src/views/GetMana.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
     <b-container>
-
       <b-row>
         <b-col cols="9">
           <NFTSelector :nfts="nfts" />
         </b-col>
 
-        <b-col cols="3">
-          Get Mana button + description
-        </b-col>
+        <b-col cols="3">Get Mana button + description</b-col>
       </b-row>
-
     </b-container>
+    <div>{{ this.wizards.config }}</div>
+    <br />
+    <div>{{ this.kitties.config }}</div>
   </div>
 </template>
 
 <script>
 import NFTSelector from "@/components/NFTSelector.vue";
 import { mapState } from "vuex";
+import axios from "axios";
 
 export default {
   name: "GetMana",
@@ -35,16 +35,54 @@ export default {
       // TODO: include tokenType
       nfts: [
         {
-          tokenId: 2445,
+          tokenId: 2445
         },
         {
-          tokenId: 2446,
+          tokenId: 2446
         },
         {
-          tokenId: 2444,
+          tokenId: 2444
         }
-      ]
+      ],
+      wizards: null,
+      kitties: null
     };
+  },
+
+  methods: {
+    getWizards(owner) {
+      axios
+        .get("https://cheezewizards.alchemyapi.io/wizards", {
+          params: {
+            owner: owner
+          },
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-token": process.env.VUE_APP_CHEEZE_VERSE_API_KEY,
+            "x-email": process.env.VUE_APP_CHEEZE_VERSE_EMAIL
+          }
+        })
+        .then(response => (this.wizards = response));
+    },
+
+    getKitties(owner) {
+      axios
+        .get("https://public.api.cryptokitties.co/v1/kitties", {
+          params: {
+            owner_wallet_address: owner
+          },
+          headers: {
+            "x-api-token": process.env.VUE_APP_KITTY_VERSE_API_TOKEN
+          }
+        })
+        .then(response => (this.kitties = response));
+    }
+  },
+
+  mounted() {
+    const owner = "";
+    this.getWizards(owner);
+    this.getKitties(owner);
   }
 };
 </script>
