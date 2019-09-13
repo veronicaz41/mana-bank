57cbbcf4d84229f2c7f1af10dd9fb05505f0bb84
Veronica Zheng <veronica.zheng@coinbase.com>
2019-09-13 16:51:27 -0700

    take getWizards and getKitties out to make development easier

--- src/views/GetMana.vue
+++ src/views/GetMana.vue
@@ -30,7 +30,7 @@
 <script>
 import NFTSelector from "@/components/NFTSelector.vue";
 import { mapGetters, mapState } from "vuex";
-//import axios from "axios";
+import { getWizards, getKitties } from "@/utils/GetNFTs.js";
 
 export default {
   name: "GetMana",
@@ -53,119 +53,6 @@ export default {
   },
 
   methods: {
-    getWizards(owner) {
-      // axios
-      //   .get("https://cheezewizards.alchemyapi.io/wizards", {
-      //     params: {
-      //       owner: owner
-      //     },
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       "x-api-token": process.env.VUE_APP_CHEEZE_VERSE_API_KEY,
-      //       "x-email": process.env.VUE_APP_CHEEZE_VERSE_EMAIL
-      //     }
-      //   })
-      //   .then(response => {
-      //     let wizards = response.data.wizards || [];
-      //     wizards.forEach(item => {
-      //       let wizard = {
-      //         id: item.id,
-      //         src: `https://storage.googleapis.com/cheeze-wizards-production/0xec2203e38116f09e21bc27443e063b623b01345a/${item.id}.svg`,
-      //         alt: `Wizard ${item.id}`,
-      //         type: "wizard"
-      //       };
-      //       this.nfts.push(wizard);
-      //     });
-      //   })
-      //   .catch(error => {
-      //     console.log(error);
-      //   });
-
-      // For testing
-      const wizards = [
-        {
-          id: 1,
-          src:
-            "https://storage.googleapis.com/cheeze-wizards-production/0xec2203e38116f09e21bc27443e063b623b01345a/1.svg",
-          type: "wizard"
-        },
-        {
-          id: 2,
-          src:
-            "https://storage.googleapis.com/cheeze-wizards-production/0xec2203e38116f09e21bc27443e063b623b01345a/2.svg",
-          type: "wizard"
-        },
-        {
-          id: 3,
-          src:
-            "https://storage.googleapis.com/cheeze-wizards-production/0xec2203e38116f09e21bc27443e063b623b01345a/3.svg",
-          type: "wizard"
-        },
-        {
-          id: 4,
-          src:
-            "https://storage.googleapis.com/cheeze-wizards-production/0xec2203e38116f09e21bc27443e063b623b01345a/4.svg",
-          type: "wizard"
-        }
-      ];
-      this.nfts.push(...wizards);
-      console.log(owner);
-    },
-
-    getKitties(owner) {
-      // axios
-      //   .get("https://public.api.cryptokitties.co/v1/kitties", {
-      //     params: {
-      //       owner_wallet_address: owner
-      //     },
-      //     headers: {
-      //       "x-api-token": process.env.VUE_APP_KITTY_VERSE_API_TOKEN
-      //     }
-      //   })
-      //   .then(response => {
-      //     // response.data { "limit": 12, "offset": 0, "kitties": [], "total": 0 }
-      //     // TODO: paging
-      //     let kitties = response.data.kitties || [];
-      //     kitties.forEach(item => {
-      //       let kitty = {
-      //         id: item.id,
-      //         src: item.image_url,
-      //         alt: `Kitty ${item.id}`,
-      //         type: "kitty"
-      //       };
-      //       this.nfts.push(kitty);
-      //       console.log(kitty);
-      //     });
-      //   })
-      //   .catch(error => {
-      //     console.log(error);
-      //   });
-
-      // For testing
-      const kitties = [
-        {
-          id: 5,
-          src:
-            "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/5.svg",
-          type: "kitty"
-        },
-        {
-          id: 6,
-          src:
-            "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/6.svg",
-          type: "kitty"
-        },
-        {
-          id: 7,
-          src:
-            "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/7.svg",
-          type: "kitty"
-        }
-      ];
-      this.nfts.push(...kitties);
-      console.log(owner);
-    },
-
     approveWizards() {
       this.drizzleInstance.contracts.WizardPresale.methods.setApprovalForAll.cacheSend(
         this.drizzleInstance.contracts.ManaBank.options.address,
@@ -208,13 +95,6 @@ export default {
     }
   },
 
-  created() {
-    // const owner = "0xF0128825b0c518858971d8521498769148137936";
-    const owner = this.activeAccount;
-    this.getWizards(owner);
-    this.getKitties(owner);
-  },
-
   mounted() {
     this.$drizzleEvents.$on("drizzle/contractEvent", async payload => {
       const { contractName, eventName, data } = payload;
@@ -230,6 +110,14 @@ export default {
   },
 
   watch: {
+    async isDrizzleInitialized() {
+      const owner = this.activeAccount;
+      const wizards = await getWizards(owner, this.drizzleInstance);
+      this.nfts.push(...wizards);
+      const kitties = await getKitties(owner, this.drizzleInstance);
+      this.nfts.push(...kitties);
+    },
+
     async selectedNFTs() {
       const manaAddress = this.drizzleInstance.contracts.ManaBank.options
         .address;
