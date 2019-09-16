0b05d822e014595bcb0f490f34550fecadbcc3b0
Veronica Zheng <veronica.zheng@coinbase.com>
2019-09-15 17:46:48 -0700

    center text

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div id="about">
     <b-container>
       <b-row>
-        <b-col cols="9">
+        <b-col>
           <h4>What</h4>
           <p>
             ManaBank is a way for CheezeWizards and CryptoKitties owners to get
@@ -14,9 +14,9 @@
             <a href="https://github.com/veronicaz41">Veronica Zheng</a> and
             <a href="https://www.lakshmansankar.com/">Lakshman Sankar</a> for
             the
-            <a href="https://coinlist.co/build/cheezewizards"
-              >CheezeWizards+CoinList hackathon</a
-            >.
+            <a
+              href="https://coinlist.co/build/cheezewizards"
+            >CheezeWizards+CoinList hackathon</a>.
           </p>
 
           <h4>How</h4>
@@ -25,13 +25,9 @@
             be redeemed by 'exiling' Kitties or Wizards. Exiling a wizard or
             kitty puts it in direct custody of the XMN contract, but grants you
             a fixed amount (currently 10) of XMN for each exiled item.
-          </p>
-
-          XMN can be:
+          </p>XMN can be:
           <ol>
-            <li>
-              used to 'summon' a random kitty or wizard held by the XMN contract
-            </li>
+            <li>used to 'summon' a random kitty or wizard held by the XMN contract</li>
             <li>
               traded anywhere you can trade ERC20s. We're starting the XMN
               economy by seeding an ETH/XMN Uniswap pool
@@ -41,7 +37,9 @@
           <!-- TODO: Add link for mainnet deploy! -->
           <p>
             The ManaBank contract is currently deployed on mainnet
-            <a href="https://etherscan.io">here</a>
+            <a
+              href="https://etherscan.io"
+            >here</a>
           </p>
 
           <h4>Why</h4>
@@ -54,7 +52,8 @@
 
           <p>
             We think that the XMN token is a step towards bridging that gap by
-            giving NFT owners something fungible <em>immediately</em> for their
+            giving NFT owners something fungible
+            <em>immediately</em> for their
             tokens. And because XMN is immediately redeemable for a random
             Wizard or Kitty that's been deposited in the contract, it's always
             guarantee to have a floor market value.
@@ -68,5 +67,9 @@
 <style scoped>
 h4 {
   padding-top: 24px;
+}
+.row {
+  margin-left: 12.5%;
+  margin-right: 12.5%;
 }
 </style>
